b707824ade14f9bea022e6341a9224ba71d0447b
Balazs Szilagyi <n0812461@ntu.ac.uk>
2020-03-22 13:16:39 +0000

    Linked list
    added Linked list

--- pixelmap.h
+++ pixelmap.h
@@ -43,4 +43,25 @@ private:
 
 };
 
+/* Linked List */
+class coordList{
+
+private:
+    typedef struct node{
+        int data;
+        node* next;
+    }* nodePtr;     /* naming and function called defined as nodePtr */
+
+    nodePtr head;
+    nodePtr curr;
+    nodePtr temp;
+
+public: /* This is where to functions goes to access private data */
+    coordList();
+    void addNode(int addData);      /* Add a node and place a certain value in it */
+    void deleteNode(int delData);   /* Whatever value is passed in here, will be searched through the list and removed */
+    void printList();               /*  */
+};
+
+
 #endif // PIXELMAP_H
