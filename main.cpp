3d35dfffb2614fa14895caaed93a420911a51537
Balazs Szilagyi <n0812461@ntu.ac.uk>
2020-04-25 15:53:28 +0100

    GUI Changes

--- main.cpp
+++ main.cpp
@@ -9,8 +9,11 @@ int main(int argc, char *argv[])
     a.setStyle("fusion");
     QDesktopWidget dw;
     MainWindow w;
-    int x = dw.width()*0.8;
-    int y = dw.height()*0.8;
+//    int x = dw.width()*0.8;
+//    int y = dw.height()*0.8;
+
+    int x = 1024;
+    int y = 768;
     w.setFixedSize(x,y);
     w.show();
     return a.exec();
